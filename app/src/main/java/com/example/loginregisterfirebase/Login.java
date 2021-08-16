6ebba29896571d7c69146db6fa5c0528339be60f
Asaf Chen <asafchen1312@gmail.com>
2021-08-16 11:16:02 +0300

    Well-Worth-App

--- app/src/main/java/com/example/loginregisterfirebase/Login.java
+++ app/src/main/java/com/example/loginregisterfirebase/Login.java
@@ -8,9 +8,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://wellworthapp-b2ced-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,8 @@ public class Login extends AppCompatActivity {
         final Button loginBtn = findViewById(R.id.loginBtn);
         final TextView registerNowBtn = findViewById(R.id.registerNowBtn);
 
+        mAuth = FirebaseAuth.getInstance();
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +50,53 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(Login.this, "Please enter your Email or Password", Toast.LENGTH_SHORT).show();
                 }
                 else{
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            // check if email is exist in firebase database
+                            if (snapshot.hasChild(emailTxt)){
+
+                                // email is exist in firebase database
+                                // now get password of user from firebase data and match it with user entered password
+
+                                final String getPassword = snapshot.child(emailTxt).child("password").getValue(String.class);
+
+                                if (getPassword.equals(passwordTxt)){
+                                    Toast.makeText(Login.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
+
+                                    // open MainActivity on success
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(Login.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(Login.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                            }
+
+                            mAuth.signInWithEmailAndPassword(emailTxt, passwordTxt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        // redirect to user profile
+                                        startActivity(new Intent(Login.this, ProfileActivity.class));
+
+
+                                    }else{
+                                        Toast.makeText(Login.this, "Failed to login!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
 
                 }
             }
